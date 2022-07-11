1b704b4dc3bab3a1515a2c6622246ecd72ce286d
sunil9813 <sunilpokhrel797@gmail.com>
2022-07-11 18:30:51 +0545

    Chip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [addTags, setAddTagas] = useState([])
+
+  const createTags = (e) => {
+    //setAddTagas([...addTags, e.target.value])
+    if (e.key === "Enter") {
+      setAddTagas([...addTags, e.target.value])
+      // to remove a create tags on click enter
+      // enter handa chai feri input chai empty garna ko lage
+      e.target.value = ""
+    }
+  }
+
+  const removeTags = (i) => {
+    // Syntax  ====> array.filter((currentValue, index, arr), thisValue)
+    setAddTagas(addTags.filter((a, index) => index !== i))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <main>
+        <div className='title'>
+          <h1>Create a Tags</h1>
+        </div>
+        <section>
+          <div className='tag'>
+            {addTags.map((val, index) => {
+              return (
+                <p key={index}>
+                  <label>{val}</label>
+                  <i className='fa fa-times' onClick={() => removeTags(index)}></i>
+                </p>
+              )
+            })}
+            <input type='text' placeholder='Enter a Key' onKeyUp={createTags} />
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
